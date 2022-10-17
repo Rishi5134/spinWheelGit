387d53ba3a5f6deb7e5ed91fdadf481aa6afa32c
Rishi Rathod <rishi3rathod5@gmail.com>
2022-10-17 15:36:16 +0530

    Removed one middleware

--- web/MongoDatabase/Controllers/tokenController.js
+++ web/MongoDatabase/Controllers/tokenController.js
@@ -1,28 +1,34 @@
 import { catchErrors } from "../MiddleWare/catchErrors.js";
 import tokenSchema from '../Schema/tokenSchema.js'
 
-export const getAccessToken =  catchErrors(async (req, res) => {
-const getToken = await tokenSchema.findOne();
-res.status(200).json({ success: true, getToken});
-})
-
-export const saveTokenToDB = async(session) => {
-    console.log("saving token to db", session);
+export const getAccessToken = async (req, res) => {
+  try {
     const getToken = await tokenSchema.findOne();
-    if(!getToken){
-  console.log("flag1");
+    res.status(200).json({ success: true, getToken });
+    
+  } catch (error) {
+    console.log("Error getting access token: " + error);
+  }
+}
 
 
-      const tok = await tokenSchema.create({accessToken:session.accessToken, shop: session.shop, id: session.id, state: session.state, isOnline: session.isOnline, scope: session.scope});
-      console.log("Created tok :-->", tok);
-    }
-    if (getToken?.accessToken !==session.accessToken || getToken.shop !== session.shop){ 
-      const updateToken = await tokenSchema.findOneAndUpdate(getToken?._id, { accessToken:session.accessToken, shop: session.shop }, {
-        new: true,
-        runValidators: false,
-        useFindAndModify: false
+export const saveTokenToDB = async (session) => {
+  console.log("saving token to db", session);
+  const getToken = await tokenSchema.findOne();
+  if (!getToken) {
+    console.log("flag1");
+
+
+    const tok = await tokenSchema.create({ accessToken: session.accessToken, shop: session.shop, id: session.id, state: session.state, isOnline: session.isOnline, scope: session.scope });
+    console.log("Created tok :-->", tok);
+  }
+  if (getToken?.accessToken !== session.accessToken || getToken.shop !== session.shop) {
+    const updateToken = await tokenSchema.findOneAndUpdate(getToken?._id, { accessToken: session.accessToken, shop: session.shop }, {
+      new: true,
+      runValidators: false,
+      useFindAndModify: false
     })
     console.log("Updated tok :-->", updateToken);
-    }
-    console.log("getToken: -->" , getToken?.accessToken);
+  }
+  console.log("getToken: -->", getToken?.accessToken);
 }
